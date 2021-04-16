25ec1aaa7171794ecd0a7623c5e769063926f1b8
dani117m <49170846+dani117m@users.noreply.github.com>
2021-04-16 15:42:52 -0500

    Update run.py

--- run.py
+++ run.py
@@ -23,12 +23,12 @@ if __name__ == "__main__":
 @app.route('/lista')
 def lista():
     return "<h1>Provincias costa</h1>"
-    return "<h2> Esmeraldas - Capital Esmeraldas 
-           "<br> Manabí - Capital Portoviejo 
-           "<br> Los Ríos - Capital Babahoyo 
-           "<br> Santa Elena - Capital Santa Elena 
-           "<br> Guayas - Capital Guayaquil    
-           "<br> Santo Domingo de los Tsáchilas - Capital Santo Domingo 
-           "<br> El Oro - Capital Machala 
+    return "<h2> Esmeraldas - Capital Esmeraldas" 
+           "<br> Manabí - Capital Portoviejo "
+           "<br> Los Ríos - Capital Babahoyo "
+           "<br> Santa Elena - Capital Santa Elena" 
+           "<br> Guayas - Capital Guayaquil"    
+           "<br> Santo Domingo de los Tsáchilas - Capital Santo Domingo "
+           "<br> El Oro - Capital Machala" 
            "</h2>"
  
