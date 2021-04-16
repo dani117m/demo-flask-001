42e8a2c0d02ab5adef8ed68d60dedb852fa7440e
dani117m <49170846+dani117m@users.noreply.github.com>
2021-04-16 15:32:03 -0500

    CostaProvincias

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return "<h1>Hola mundo con Flask ñevantado desde heroku</h1>"
+    return "<h1>Hola mundo con Flask levantado desde heroku</h1>"
     return "<h1>dani117m</h1>"
 
 @app.route('/suma')
@@ -19,3 +19,14 @@ def suma():
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
+    
+@app.route('/lista')
+def lista():
+    return "<h2>Provincias costa</h2>"
+    return "<p> Esmeraldas - Capital Esmeraldas </p>"
+           "<p> Manabí - Capital Portoviejo </p>"
+           "<p> Los Ríos - Capital Babahoyo </p>"
+           "<p> Santa Elena - Capital Santa Elena </p>"
+           "<p> Guayas - Capital Guayaquil </p>"
+           "<p> Santo Domingo de los Tsáchilas - Capital Santo Domingo </p>"
+           "<p> El Oro - Capital Machala </p>"
